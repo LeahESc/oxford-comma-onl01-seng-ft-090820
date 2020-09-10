e4fb7a603ed96862b65be6aefd2b5f0ca7f24ebd
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 21:29:24 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -5,6 +5,6 @@ def oxford_comma(array)
     return array.join(" and ")
     elsif array.size >= 3
     arr = array.pop
-     return arr.join(", ") + "and" + array[-1]
+     return array.join(", ") + "and" + arr
   end
 end
