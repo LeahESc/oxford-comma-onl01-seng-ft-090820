260576575ba300690aa12c58d7c15f13a10c2616
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 20:49:51 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,5 +1,5 @@
 def oxford_comma(array)
- 
-default_words_connector     = ", "
+    default_words_connector     = ", "
     default_two_words_connector = " and "
     default_last_word_connector = ", and "
+end
