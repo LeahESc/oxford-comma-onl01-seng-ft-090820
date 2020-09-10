80f689677adacd3c56908695ff219f7aab3abb51
Leah Schlackman <leahemily1@gmail.com>
2020-09-10 20:57:50 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,11 @@
 def oxford_comma(array)
     if length = 1
     return array[0]
-    elsif length = 2 
+  end
+end
+
+def oxford_comma(array)
+    if length = 2 
     return array.join(" and ")
   end
 end
